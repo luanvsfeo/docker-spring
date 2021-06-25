156fd4c16bc0c762c4b8050fe23ec8f8460e9bb2
Luan Feo <luanfeo@gmail.com>
2021-06-25 13:53:18 -0300

    teste-impl - Alteração dos nomes dos metodos

--- src/main/java/com/docker/spring_boot/repository/OrderRepository.java
+++ src/main/java/com/docker/spring_boot/repository/OrderRepository.java
@@ -1,6 +1,6 @@
 package com.docker.spring_boot.repository;
 
-import com.docker.spring_boot.domain.Customer;
+import com.docker.spring_boot.domain.User;
 import com.docker.spring_boot.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface OrderRepository extends JpaRepository<Order,Long> {
 
-	Order findByCustomerAndOrderDateIsNullAndDeleteDateIsNull(Customer customer);
+	Order findByUserAndOrderDateIsNullAndDeleteDateIsNull(User user);
 
-	boolean existsByCustomerAndOrderDateIsNullAndDeleteDateIsNull(Customer customer);
+	boolean existsByUserAndOrderDateIsNullAndDeleteDateIsNull(User user);
 }

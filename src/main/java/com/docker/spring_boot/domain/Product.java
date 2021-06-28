576a6f6887978d0844b2985b0e601c44063a6293
Luan Feo <luanfeo@gmail.com>
2021-06-28 13:38:09 -0300

    master - inclusão de construtor

--- src/main/java/com/docker/spring_boot/domain/Product.java
+++ src/main/java/com/docker/spring_boot/domain/Product.java
@@ -49,6 +49,13 @@ public class Product {
 	public Product() {
 	}
 
+	public Product(Long id, String name, String description, Double price) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+	}
+
 	public Long getId() {
 		return id;
 	}

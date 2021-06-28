b3e3a076047983384ba2062e3d2c19f28f0dd63b
Luan Feo <luanfeo@gmail.com>
2021-06-28 13:38:28 -0300

    master - inclusão de metodos

--- src/main/java/com/docker/spring_boot/repository/ProductRepository.java
+++ src/main/java/com/docker/spring_boot/repository/ProductRepository.java
@@ -4,6 +4,12 @@ import com.docker.spring_boot.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long> {
+
+	boolean existsByIdAndDeletedAtIsNull(Long id);
+
+	List<Product> findAllByDeletedAtIsNull();
 }
